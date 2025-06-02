ced0b490718a3f6b5a503e11aecdf630c6e9f1f1
minubyte <seomeenu@proton.me>
2025-06-02 23:27:02 +0900

    camera 추가

--- src/scenes/game.lua
+++ src/scenes/game.lua
@@ -6,12 +6,36 @@ local Player = require("src.objects.player")
 
 function Game:init()
     self:add(Player)
+
+    self.camera_shake = {
+        dur = 0,
+        x = 0,
+        y = 0,
+    }
+    self.camera = {
+        x = 0,
+        y = 0,
+    }
 end
 
 function Game:draw()
+    love.graphics.push()
+
+    if self.camera_shake.dur > 0.1 then
+        love.graphics.translate(self.camera_shake.x, self.camera_shake.y)
+    end
+
+    Res:apply()
+    
     for _, object in pairs(self.objects) do
         object:draw()
     end
+
+    love.graphics.pop()
+end
+
+function Game:shake(dur)
+    self.camera_shake.dur = dur
 end
 
 function Game:update(dt)
