34427e4cff07f2f996148f2f38b12bb1cc23cc2d
minubyte <seomeenu@proton.me>
2025-07-15 18:50:46 +0900

    rgba 수정

--- src/utils.lua
+++ src/utils.lua
@@ -1,8 +1,8 @@
 function rgb(r, g, b)
-    return {love.math.colorFromBytes(r, g, b)}
+    return {r/255, g/255, b/255}
 end
 function rgba(r, g, b, a)
-    return {love.math.colorFromBytes(r, g, b, a)}
+    return {r/255, g/255, b/255, a}
 end
 function a(rgb, a)
     return {rgb[1], rgb[2], rgb[3], a}
